4041b1d8272b370c171364d9594bdbf7d440ec4f
MaisaBanyIssa <maisabanyissa@gmail.com>
2023-06-04 19:23:14 +0300

    update

---
+++ app.js
@@ -0,0 +1,18 @@
+/*
+
+*/
+for (let i =0; i<10 ;i++) {
+    
+   if (i%2==0  ) {
+    console.log(i)
+   } else if (i==5) {
+     continue;
+    // let c=10
+    
+   } else{
+    console.log("odd" +i)
+   }
+        
+} 
+
+//console.log(5%2)
